9438093151b9445f36a48bfe8b25aa94d2b3de4a
SofiaNechaeva <sonyarepman0803@gmail.com>
2024-12-11 21:16:09 +0300

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -22,8 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'corsheaders',
-    'rest_framework',
-    'api'
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
